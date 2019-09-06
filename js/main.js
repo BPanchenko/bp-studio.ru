5e808363e51e323ca45b4f8d9f528109e148ca4a
Boris Panchenko <oberon@list.ru>
2019-09-06 07:30:50 +0300

    fetch and get random principle

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 
-import * as THREE from './three.module.js';
+import * as THREE from './three.module.js'
+
+const PRINCIPLES_PATH = "/principles.json"
+
+const elems = initElems()
+
+
+/* Principle: fetch & render
+ ========================================================================== */
+
+async function fetchPrinciple() {
+    let principlesList = await fetch(PRINCIPLES_PATH)
+        .then(response => response.json())
+        .then(json => json.data.filter(d => d.enable))
+    return _.sample(principlesList)
+}
+
+function renderPrinciple(data) {
+    console.log(data)
+}
+
+fetchPrinciple().then(renderPrinciple)
+
+
+/* Helpers
+ ========================================================================== */
+
+function initElems() {
+    return {
+        principleText: document.querySelector('.js-principle-text'),
+        principleTitle: document.querySelector('.js-principle-title')
+    };
+}

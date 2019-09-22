cbaa0b1ab73d900c11fcb9d72bf2cb4b71b2a0da
Boris Panchenko <oberon@list.ru>
2019-09-22 15:48:49 +0300

    + `Arrows` render function

---
+++ js/pics/arrows.js
@@ -0,0 +1,81 @@
+
+import {
+    THREE,
+    scene
+} from '../init.js'
+
+const EXTRUDE_SETTINGS = {
+    depth: 8,
+    bevelEnabled: true,
+    bevelSegments: 2,
+    steps: 2,
+    bevelSize: 1,
+    bevelThickness: 1
+}
+
+function initBiggerArrow() {
+
+    let pts = [
+        new THREE.Vector2( 0.923728813559322 , 0.7768361581920904 ),
+        new THREE.Vector2( -0.5084745762711864 , 0.7768361581920904 ),
+        new THREE.Vector2( -0.0451977401129944 , 0.3050847457627119 ),
+        new THREE.Vector2( -0.7514124293785311 , -0.403954802259887 ),
+        new THREE.Vector2( -0.2598870056497175 , -0.8983050847457627 ),
+        new THREE.Vector2( 0.4519774011299435 , -0.1892655367231638 ),
+        new THREE.Vector2( 0.923728813559322 , -0.6553672316384181 ),
+        new THREE.Vector2( 0.923728813559322 , 0.7768361581920904 )
+    ]
+
+    let shape = new THREE.Shape(pts)
+    let geometry = new THREE.ShapeGeometry(shape)
+    let material = new THREE.MeshBasicMaterial( {
+        color: 0x87c4be
+    } )
+    let mesh = new THREE.Mesh(geometry, material)
+
+    return mesh
+}
+
+function initSmallerArrow() {
+    let pts = [
+        new THREE.Vector2( -0.2598870056497175 , -0.8983050847457627 ),
+        new THREE.Vector2( 0.3813559322033898 , -0.8983050847457627 ),
+        new THREE.Vector2( 0.172316384180791 , -0.6864406779661017 ),
+        new THREE.Vector2( 0.8305084745762712 , -0.0254237288135593 ),
+        new THREE.Vector2( 0.6242937853107345 , 0.1836158192090395 ),
+        new THREE.Vector2( -0.0367231638418079 , -0.480225988700565 ),
+        new THREE.Vector2( -0.2598870056497175 , -0.1949152542372881 ),
+        new THREE.Vector2( -0.2598870056497175 , -0.8983050847457627 )
+    ]
+
+    let shape = new THREE.Shape(pts)
+    let geometry = new THREE.ShapeGeometry(shape)
+    let material = new THREE.MeshBasicMaterial( {
+        color: 0xbb3907,
+        opacity: 0.7
+    } )
+
+    material.blending = THREE.CustomBlending;
+    material.blendEquation = THREE.AddEquation;
+    material.blendSrc = THREE.ZeroFactor;
+
+    // material.blendSrc = THREE.OneFactor
+    // material.blendSrc = THREE.SrcColorFactor
+    // material.blendSrc = THREE.OneMinusSrcColorFactor
+    // material.blendSrc = THREE.SrcAlphaFactor
+    // material.blendSrc = THREE.OneMinusSrcAlphaFactor
+    // material.blendSrc = THREE.DstAlphaFactor
+    // material.blendSrc = THREE.OneMinusDstAlphaFactor
+    material.blendSrc = THREE.DstColorFactor
+    // material.blendSrc = THREE.OneMinusDstColorFactor
+    // material.blendSrc = THREE.SrcAlphaSaturateFactor
+    
+    let mesh = new THREE.Mesh(geometry, material)
+
+    return mesh
+}
+
+export default function () {
+    scene.add(initBiggerArrow())
+    scene.add(initSmallerArrow())
+}

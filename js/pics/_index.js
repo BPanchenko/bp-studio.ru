f618395f5cfb4e8f5bd5dcc3d98c1ad150a80f4b
Boris Panchenko <oberon@list.ru>
2019-09-25 08:14:14 +0300

    + index file with includes render functions

---
+++ js/pics/_index.js
@@ -0,0 +1,16 @@
+
+import arrows from './arrows.js'
+import circles from './circles.js'
+import parallelepipedAndCircle from './parallelepiped-and-circle.js'
+import semicircles from './semicircles.js'
+import squares from './squares.js'
+import squaresAndCircle from './squares-and-circle.js'
+
+export default {
+    arrows,
+    circles,
+    parallelepipedAndCircle,
+    semicircles,
+    squares,
+    squaresAndCircle
+}

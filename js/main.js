c9deb3f5e8ab41368c8a932a89b6ddb13761e197
Boris Panchenko <oberon@list.ru>
2019-09-26 07:24:28 +0300

    fix duplicate items when randomly selecting a list item

--- js/main.js
+++ js/main.js
@@ -3,32 +3,39 @@ import {
     CLS,
     ELEMS,
     PRINCIPLES_PATH,
-    renderScene
+    renderScene,
+    scene
 } from './init.js'
 
 import pics from './pics/_index.js'
 
 
-/* ========================================================================
-   Canvas
- ========================================================================== */
+let picsList = _.values(pics)
+let principlesList
+let getAndRenderPrinciple = _.flow([getRandomItem, renderPrinciple])
 
-pics.semicircles()
-renderScene()
-
-
-/* ========================================================================
-   Principle: fetch & render
- ========================================================================== */
-
-async function fetchPrinciple() {
-    let principlesList = await fetch(PRINCIPLES_PATH)
+async function fetchPrinciples() {
+    principlesList = await fetch(PRINCIPLES_PATH)
         .then(response => response.json())
         .then(json => json.data.filter(d => d.enable))
-    return _.sample(principlesList)
+    return principlesList
+}
+
+function getRandomItem(list) {
+    let data = _.sample(list.filter(d => !d.marked))
+    if (data) {
+        data.marked = true
+        return data
+    } else {
+        list.forEach(d => d.marked = false)
+        return getRandomItem(list)
+    }
+    
 }
 
 function renderPrinciple(data) {
+    
+    // content
 
     if (data.title.length <= 14) {
         ELEMS.principleTitle.classList.add(CLS.bigger);
@@ -39,9 +46,15 @@ function renderPrinciple(data) {
     ELEMS.principleTitle.innerHTML = data.title
     ELEMS.principleText.innerHTML = data.text
 
+    // picture
+
+    for( var i = scene.children.length - 1; i >= 0; i--) scene.remove(scene.children[i])
+    getRandomItem(picsList)()
+    renderScene()
+
 }
 
-fetchPrinciple().then(renderPrinciple)
+fetchPrinciples().then(getAndRenderPrinciple)
 
 // TODO: 
 // 1) получение данных с сервера выполнять только когда их нет в приложении;
@@ -50,4 +63,4 @@ fetchPrinciple().then(renderPrinciple)
 /* DOM Events
  ========================================================================== */
 
-document.body.addEventListener('click', event => fetchPrinciple().then(renderPrinciple), false);
+document.body.addEventListener('click', evt => getAndRenderPrinciple(principlesList), false);

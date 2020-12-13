bd35120435f1d50d22707fd58671c40680387a03
Boris Panchenko <bpanchenko@aligntech.com>
2020-12-13 13:46:30 +0300

    item's container (logical fallacy)

--- js/logical-fallacies.js
+++ js/logical-fallacies.js
@@ -1,8 +1,23 @@
 const DATA_PATH = "/logical-fallacies.json"
 
+function createItemElement(item) {
+    let elem = document.createElement('div')
+    elem.classList.add('item-container')
+    return (item.elem = elem)
+}
+
 async function fetchData() {
     let result = await fetch(DATA_PATH).then(response => response.json())
     return result
 }
 
-fetchData().then(data => console.log(data))
+function getItemElement(item) {
+    return item.elem ?? createItemElement(item)
+}
+
+function renderList(list) {
+    list.forEach(item => item => container.appendChild(getItemElement(item)));
+    return listContainer;
+}
+
+fetchData().then(responseJson => renderList(responseJson.data))

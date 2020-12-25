31218c87e1afa8c84006b156540e2c2f28aacc89
Boris Panchenko <oberon@list.ru>
2020-12-25 22:51:50 +0300

    rendering shuffled fallacies

--- js/logical-fallacies.js
+++ js/logical-fallacies.js
@@ -1,14 +1,23 @@
 {
   const DATA_PATH = "/logical-fallacies.json"
+  const TPL = document.querySelector('.js-tpl-fallacy')
 
   function createItemElement(item) {
+    let content = document.importNode(TPL.content, true)
+    content.querySelector('.js-title').innerHTML = item.title
+    content.querySelector('.js-description').innerHTML = item.description
+    content.querySelector('.js-text').innerHTML = item.text
+
     let elem = document.createElement('div')
     elem.classList.add('c-list__item', 'o-fallacy-container')
+    elem.appendChild(content)
+
     return (item.elem = elem)
   }
 
   async function fetchData() {
-    let result = await fetch(DATA_PATH).then(response => response.json())
+    let response = await fetch(DATA_PATH).then(response => response.json())
+    let result = shuffle(response.data)
     return result
   }
 
@@ -20,9 +29,19 @@
     list.forEach(item => container.appendChild(getItemElement(item)))
     return container
   }
+  
+  function shuffle(arr) {
+    let j, t
+    for (let i = arr.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random()*(i + 1))
+      t = arr[j]; arr[j] = arr[i]; arr[i] = t
+    }
+    return arr;
+  }
 
-  document.addEventListener("DOMContentLoaded", event => {
+  document.addEventListener("DOMContentLoaded", async event => {
     let listContainer = document.querySelector('.js-list')
-    fetchData().then(responseJson => renderList(listContainer, responseJson.data))
+    let response = await fetchData()
+    renderList(listContainer, response)
   })
 }

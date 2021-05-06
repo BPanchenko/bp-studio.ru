42a2b31076766ea3516dc08a0cde49fc1fc264b0
Boris Panchenko <oberon@list.ru>
2021-05-06 06:53:01 +0300

    stop bubling of click by the nav control

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 {
     let elNav, elNavControl
 
-    function onClickNavControl (event) {
-        event.preventDefault()
+    function onClickNavControl (evt) {
+        evt.preventDefault()
+        evt.stopPropagation()
         let expanded = elNavControl.getAttribute('aria-expanded') !== 'true'
         elNav.setAttribute('aria-expanded', expanded)
         elNavControl.setAttribute('aria-expanded', expanded)
     }
 
-    document.addEventListener('DOMContentLoaded', event => {
+    document.addEventListener('DOMContentLoaded', evt => {
         elNav = document.querySelector('.js-nav')
         elNavControl = document.querySelector('.js-nav-control')
         elNavControl.addEventListener('click', onClickNavControl)

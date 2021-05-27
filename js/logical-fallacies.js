fab6162212e04693ea836a3e58283337a46495ed
Boris Panchenko <oberon@list.ru>
2021-05-27 06:28:32 +0300

    codestyle

--- js/logical-fallacies.js
+++ js/logical-fallacies.js
@@ -1,47 +1,47 @@
 {
-  const DATA_PATH = "/data/logical-fallacies.json"
-  const TPL = document.querySelector('.js-tpl-fallacy')
+    const DATA_PATH = "/data/logical-fallacies.json"
+    const TPL = document.querySelector('.js-tpl-fallacy')
 
-  function createItemElement(item) {
-    let content = document.importNode(TPL.content, true)
-    content.querySelector('.js-title').innerHTML = item.title
-    content.querySelector('.js-description').innerHTML = item.description
-    content.querySelector('.js-example').innerHTML = item.example
+    document.addEventListener("DOMContentLoaded", async event => {
+        let listContainer = document.querySelector('.js-list')
+        let response = await fetchData()
+        renderList(listContainer, response)
+    })
 
-    let elem = document.createElement('div')
-    elem.classList.add('c-list__item', 'o-fallacy-container')
-    elem.appendChild(content)
-
-    return (item.elem = elem)
-  }
-
-  async function fetchData() {
-    let response = await fetch(DATA_PATH).then(response => response.json())
-    let result = shuffle(response.data)
-    return result
-  }
-
-  function getItemElement(item) {
-    return item.elem ?? createItemElement(item)
-  }
-
-  function renderList(container, list) {
-    list.forEach(item => container.appendChild(getItemElement(item)))
-    return container
-  }
-  
-  function shuffle(arr) {
-    let j, t
-    for (let i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random()*(i + 1))
-      t = arr[j]; arr[j] = arr[i]; arr[i] = t
+    async function fetchData() {
+        let response = await fetch(DATA_PATH).then(response => response.json())
+        let result = shuffle(response.data)
+        return result
     }
-    return arr;
-  }
 
-  document.addEventListener("DOMContentLoaded", async event => {
-    let listContainer = document.querySelector('.js-list')
-    let response = await fetchData()
-    renderList(listContainer, response)
-  })
+    function createItemElement(item) {
+        let content = document.importNode(TPL.content, true)
+        content.querySelector('.js-title').innerHTML = item.title
+        content.querySelector('.js-description').innerHTML = item.description
+        content.querySelector('.js-example').innerHTML = item.example
+
+        let elem = document.createElement('div')
+        elem.classList.add('c-list__item', 'o-fallacy-container')
+        elem.appendChild(content)
+
+        return (item.elem = elem)
+    }
+
+    function getItemElement(item) {
+        return item.elem ?? createItemElement(item)
+    }
+
+    function renderList(container, list) {
+        list.forEach(item => container.appendChild(getItemElement(item)))
+        return container
+    }
+
+    function shuffle(arr) {
+        let j, t
+        for (let i = arr.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random()*(i + 1))
+            t = arr[j]; arr[j] = arr[i]; arr[i] = t
+        }
+        return arr
+    }
 }
